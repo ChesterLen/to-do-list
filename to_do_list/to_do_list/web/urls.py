f4046c698ab46e2006dbbde8ad7adcabaedb46cb
unknown <svetoslavbaykov55@gmail.com>
2025-03-27 11:36:11 +0200

    API created

--- to_do_list/to_do_list/web/urls.py
+++ to_do_list/to_do_list/web/urls.py
@@ -4,4 +4,5 @@ from to_do_list.web import views
 
 urlpatterns = [
     path('', views.CreateTaskView.as_view(), name='create_task'),
+    path('task/api/view/', views.RestApiTaskView.as_view(), name='task_api_view'),
 ]
--- to_do_list/to_do_list/web/views.py
+++ to_do_list/to_do_list/web/views.py
@@ -2,6 +2,18 @@ from rest_framework import serializers
 from django.views import generic as views
 from to_do_list.web import forms, models
 from django.urls import reverse_lazy
+from rest_framework import generics as api_views
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Task
+        fields = '__all__'
+
+
+class RestApiTaskView(api_views.ListAPIView):
+    serializer_class = TaskSerializer
+    queryset = models.Task.objects.all()
 
 
 class CreateTaskView(views.CreateView):

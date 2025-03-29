312b2089e1f030a2e2e00a15adcad6f0ec9e0552
unknown <svetoslavbaykov55@gmail.com>
2025-03-29 15:33:03 +0200

    HTML/CSS implementation

--- to_do_list/to_do_list/settings.py
+++ to_do_list/to_do_list/settings.py
@@ -88,7 +88,7 @@ USE_I18N = True
 USE_TZ = True
 
 
-STATIC_URL = '/to_do_list/static/'
-STATICFILE_DIRS = [os.path.join(BASE_DIR / 'to_do_list/static')]
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static/']
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
